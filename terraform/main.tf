56acd65d9c8499571bbe87401c7d431e9d3f5c93
precharat-chu <precharat.chu@snocko-tech.com>
2023-04-11 14:03:20 +0700

    deploy terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,14 +9,75 @@ terraform {
   }
 }
 
+locals {
+  bucket_name = "playground-react-s3"
+}
+
 provider "aws" {
   region = "ap-southeast-1"
 }
+
+# resource "aws_iam_role" "this" {
+#   assume_role_policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Action": "sts:AssumeRole",
+#       "Principal": {
+#         "Service": "ec2.amazonaws.com"
+#       },
+#       "Effect": "Allow",
+#       "Sid": ""
+#     }
+#   ]
+# }
+# EOF
+# }
+
+# data "aws_iam_policy_document" "bucket_policy" {
+#   statement {
+#     principals {
+#       type        = "AWS"
+#       identifiers = [aws_iam_role.this.arn]
+#     }
+
+#     actions = [
+#       "s3:ListBucket",
+#     ]
+
+#     resources = [
+#       "arn:aws:s3:::${local.bucket_name}",
+#     ]
+#   }
+# }
+
 
 module "s3-bucket" {
   source  = "app.terraform.io/precharat-chu/s3-bucket/aws"
   version = "3.6.0"
 
-  bucket_prefix = "playground-react-s3"
+  bucket_prefix = local.bucket_name
+
+  # attach_policy                         = true
+  # policy                                = data.aws_iam_policy_document.bucket_policy.json
+  # attach_deny_insecure_transport_policy = true
+  # attach_require_latest_tls_policy      = true
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+}
+
+website = {
+  # conflicts with "error_document"
+  #        redirect_all_requests_to = {
+  #          host_name = "https://modules.tf"
+  #        }
+
+  index_document = "index.html"
+  error_document = "error.html"
 
 }
